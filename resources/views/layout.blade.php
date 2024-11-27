dc86aa415dd53bda5609734e13fe5f8352a81065
Rohit Chandra <chandrarohit91@gmail.com>
2024-11-27 17:56:25 +0530

    Add new components and styles for navigation and hero sections

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,16 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
-    <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
+        integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 
 </head>
 
 <body class="bg-gray-100">
     <x-header />
-    <main class="container mx-auto p-4 mt-4 bg-blue-400 ">
-        {{ $slot }}
+    @if (request()->is('/'))
+        <x-hero />
+    @endif
+
+    {{ $slot }}
     </main>
+    <script src="{{ asset('/js/script.js') }}"></script>
 </body>
 
 </html>
